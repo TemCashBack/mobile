39b44ec6854622afa59cd09a48e787888eb5f790
Enzo Nagata <enzo.nagata@gmail.com>
2025-09-08 20:00:24 -0300

    atualiza APIKeys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import Flutter
 import UIKit
-import CoreLocation
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,36 +8,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    
-    // Configuração específica para localização no iOS
-    if #available(iOS 14.0, *) {
-      let locationManager = CLLocationManager()
-      locationManager.delegate = self
-    }
-    
-    // Configuração específica para Google Maps
-    GMSServices.provideAPIKey("AIzaSyC6Q5EC_3hYvdCpkMGje4Kv2z5_mE12fkE")
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-// MARK: - CLLocationManagerDelegate
-extension AppDelegate: CLLocationManagerDelegate {
-  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-    switch status {
-    case .authorizedWhenInUse, .authorizedAlways:
-      print("Localização autorizada")
-    case .denied, .restricted:
-      print("Localização negada ou restrita")
-    case .notDetermined:
-      print("Localização não determinada")
-    @unknown default:
-      print("Status de localização desconhecido")
-    }
-  }
-  
-  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    print("Erro na localização: \(error.localizedDescription)")
   }
 }
